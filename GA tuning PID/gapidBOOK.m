52ea6b90f6a99a3e865fcbb11a4bf18b30bc051e
qiaolinhan <742954173@qq.com>
2019-11-20 22:30:06 -0500

    Update gapidBOOK.m

--- GA tuning PID/gapidBOOK.m
+++ GA tuning PID/gapidBOOK.m
@@ -1,4 +1,4 @@
-'''
+```
 % 1. define a system-->some transfer function
 % 2. define some objective fucntion 'J'
 % 3. wrap this GA command around this
@@ -12,14 +12,9 @@ MaxGeneration=10; %10 generation
 
 % we want to find the system we are going to optimize the PID around, so start with a transfer function of S.
 s=tf('s');
-
 % the system G
 G=1/(s*(s*s+s+1))
 
-
-%state_dot=states_model(states,inputs); % dynamical system
-
-options=optimoptions(@ga,'Populationsize',Popsize,'MaxGenerations',MaxGenerations)
-
-[x,fval]=ga(@(K)pidtest(G,dt,K),3-eye(3),zeros(3,1))
-'''
+options=optimoptions(@ga,'Populationsize',Popsize,'MaxGenerations',MaxGenerations,'OutputFcn',@myfun)
+[x,fval]=ga(@(K)pidtest(G,dt,K),3-eye(3),zeros(3,1)) % ga: use GA. @(K): what parameter to optimize, so wrap 'pidtest' snd login G and dt
+```
