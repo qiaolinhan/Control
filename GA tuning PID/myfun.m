a1af3d0ac6f83c6d72e27b619201de399edd8081
qiaolinhan <742954173@qq.com>
2019-11-20 22:41:53 -0500

    Create myfun.m

---
+++ GA tuning PID/myfun.m
@@ -0,0 +1,23 @@
+
+% This is besically and every iteration and every time we evaluate an individual and every time the generation advances.
+% save some inertial variables so that we can plot them later.
+
+function [state, options,optchanged] = myfun(options,state,flag)
+persistent history 
+persistent cost
+optchanged = false;
+
+switch flag
+ case 'init'
+        history(:,:,1) = state.Population;
+        cost(:,1) = state.Score;
+    case {'iter','interrupt'}
+        ss = size(history,3);
+        history(:,:,ss+1) = state.Population;
+        cost(:,ss+1) = state.Score;
+    case 'done'
+        ss = size(history,3);
+        history(:,:,ss+1) = state.Population;
+        cost(:,ss+1) = state.Score;
+        save history.mat history cost
+end
